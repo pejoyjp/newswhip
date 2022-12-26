b9d6adbad579fbf747755696a7ee9df01b18ba53
pejoyjp <jinpeng8111765@mail.com>
2022-12-26 18:13:31 +0000

    jin

--- koobamock/src/components/Header.jsx
+++ koobamock/src/components/Header.jsx
@@ -6,8 +6,6 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
-import Navbar from "./Navbar.jsx";
-
 
 const MyIconButton = styled(IconButton)({
     color:'rgb(4,251,252)',
